dd8ceb97c7ac834b6a5ad98a060521c5b164d135
Francisco Barrento <francisco.barrento@gmail.com>
2025-07-23 19:03:20 +0100

    infra(docker): don't create .env file

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -7,13 +7,6 @@ if [ ! -f vendor/autoload.php ]; then
   composer install --no-progress --no-interaction
 fi
 
-if [ ! -f .env ]; then
-  echo "Creating env file for new $APP_ENV"
-  cp .env.example .env
-else
-  echo "env file already exists"
-fi
-
 if [ -d storage ]; then
   echo "Setting storage permissions"
   chmod -R 755 storage
