85e7d8a7d62236b92a559124c1be6536ba1f625a
Francisco Barrento <francisco.barrento@gmail.com>
2025-07-27 19:19:21 +0100

    chore(reverb): setting reverb variables

--- admin/app/Providers/Filament/AdminPanelProvider.php
+++ admin/app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Http\Middleware\EnsureSessionPath;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -45,6 +46,7 @@ class AdminPanelProvider extends PanelProvider
                 FilamentInfoWidget::class,
             ])
             ->middleware([
+                EnsureSessionPath::class,
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
                 StartSession::class,
